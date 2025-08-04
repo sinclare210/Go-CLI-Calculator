324be152d73db530b6867f8a262afb72a00b6f55
sinclare210 <adebayooluwasegun2004@gmail.com>
2025-08-04 16:41:55 +0100

    An issue came up with the user input, so i had to add the for loop so that the user would not proceed to the nest stage if he doesnt put a value between 1-4

--- main.go
+++ main.go
@@ -26,21 +26,34 @@ func main() {
 	var arith int
 	var a int
 	var b int
-	fmt.Println("👋 Hi there! Ready to do some quick math?")
-	fmt.Println("What arithmetic do you want to perform today?")
-	fmt.Println("{1 for Add} {2 for Subtract} {3 for Multiply} {4 for Division}?")
-	fmt.Scanln(&arith)
+
+	for{
+
+		fmt.Println("👋 Hi there! Ready to do some quick math?")
+		fmt.Println("What arithmetic do you want to perform today?")
+		fmt.Println("{1 for Add} {2 for Subtract} {3 for Multiply} {4 for Division}?")
+		fmt.Scanln(&arith)
+
+		
+		if arith >= 1 && arith <= 4 {
+			break
+		}
+		fmt.Println("Please enter a number between 1 and 4.")
+	}
+
+	
 	fmt.Println("Add the two numbers you want to work with")
 	fmt.Println("First Number")
 	fmt.Scanln(&a)
 	fmt.Println("Second Number")
 	fmt.Scanln(&b)
+	
 
 	fmt.Println("The Ans is")
 
 
 	switch arith {
-			case 1:
+	case 1:
 		ans := add(a,b)
 		fmt.Println(ans)
 
